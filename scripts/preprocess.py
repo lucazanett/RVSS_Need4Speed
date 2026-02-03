5f231fbebfc2762e5c0e865e2484bf7067bafa22
Luca Zanetti <luca.zanetti@iit.it>
2026-02-04 00:03:48 +1100

    added

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -2,25 +2,27 @@ import torch
 import torchvision.transforms as transforms
 import numpy as np  
 import cv2
-def preprocess_image(image):
+
+
+class PreProcessImage(object):
 
     
+    def __call__(self, *args, **kwds):
+        image = np.array(image) 
 
-    image = np.array(image) 
 
-
-    # B. COLOR SPACE CONVERSION (RGB -> YUV)
-    # YUV separates brightness (Y) from color (UV), helping with shadows
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
-    
-    # C. GAUSSIAN BLUR
-    image = cv2.GaussianBlur(image, (3, 3), 0)
-    
-    # D. RESIZE
-    # Resize to NVIDIA standard (200 width, 66 height)
-    
-    # E. NORMALIZE
-    # Normalize to [0, 1] range
-    image = image / 255.0 
-    image = image.astype(np.float32)
-
+        # B. COLOR SPACE CONVERSION (RGB -> YUV)
+        # YUV separates brightness (Y) from color (UV), helping with shadows
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
+        
+        # C. GAUSSIAN BLUR
+        image = cv2.GaussianBlur(image, (3, 3), 0)
+        
+        # D. RESIZE
+        # Resize to NVIDIA standard (200 width, 66 height)
+        
+        # E. NORMALIZE
+        # Normalize to [0, 1] range
+        image = image / 255.0 
+        image = image.astype(np.float32)
+        return image
